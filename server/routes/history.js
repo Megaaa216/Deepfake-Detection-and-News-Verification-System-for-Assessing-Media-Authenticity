33c5f11f32d9edef5667922336227cc1df3aa491
Vuvietanh <anhviet.vu2006@gmail.com>
2026-06-18 09:02:02 +0700

    1

--- server/routes/history.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const historyController = require('../controllers/historyController');
-const auth = require('../middleware/authMiddleware');
-
-router.get('/', auth, historyController.getAll);
-router.get('/:id', auth, historyController.getById);
-
-module.exports = router;
